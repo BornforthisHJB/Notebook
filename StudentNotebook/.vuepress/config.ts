6e9c95fcd40b783cc38d81135a7aec2b169f868c
AndersonHJB <bornforthis@bornforthis.cn>
2022-08-07 21:51:26 +0800

    update

--- StudentNotebook/.vuepress/config.ts
+++ StudentNotebook/.vuepress/config.ts
@@ -11,7 +11,12 @@ export default defineUserConfig({
 
   plugins: [
     searchPlugin({
-      // 你的选项
+      locales: {
+        "/": {
+          placeholder: "搜索",
+          
+        },
+      },
     }),
   ],
 
